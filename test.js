cb413667f089ac6c77c3633f0647dcb520a368da
Jed Papa <jdpapa@dbpsc.com.ph>
2024-05-23 22:56:46 +0800

    minor syntax changes

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-require('http').createServer(function(req,res){
-    
+require('http').createServer(function(req,res) {
+
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
