13d586312feac3251a8b59dcbfa92c723c1add20
Jed Papa <jdpapa@dbpsc.com.ph>
2024-05-23 15:54:17 +0800

    updated SSID

--- gps_neo6m.ino
+++ gps_neo6m.ino
@@ -14,8 +14,8 @@ const analytics = getAnalytics(app);
 TinyGPSPlus gps;
 SoftwareSerial SerialGPS(4, 5); 
 
-const char* ssid = "Reno4";
-const char* password = "123456789";
+const char* ssid = "OPPO A96";
+const char* password = "GwaponiJed";
 
 float Latitude , Longitude;
 int year , month , date, hour , minute , second;
